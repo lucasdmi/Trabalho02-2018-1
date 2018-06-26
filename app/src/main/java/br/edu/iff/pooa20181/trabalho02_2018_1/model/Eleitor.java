b805df6dc9b0f95e79c0c84638bca4d312667b94
Lucas <lucasmisidoro@gmail.com>
2018-06-26 10:25:44 -0300

    Implementando  a classe Modelo Eleitor

---
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/model/Eleitor.java
@@ -0,0 +1,58 @@
+package br.edu.iff.pooa20181.trabalho02_2018_1.model;
+
+import java.util.Date;
+
+public class Eleitor {
+
+    private int id;
+
+
+    private String nome;
+    private String nomeMae;
+    private Date dataNascimento;
+
+    public Eleitor(){
+
+    }
+
+    public Eleitor(int id, String nome, String nomeMae, Date dataNascimento){
+        this.id = id;
+        this.nome = nome;
+        this.nomeMae = nomeMae;
+        this.dataNascimento = dataNascimento;
+
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
+
