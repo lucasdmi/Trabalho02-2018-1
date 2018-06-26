79ac9ff8dbb8b6f0d03d292a92f9a5edab3da752
Lucas <lucasmisidoro@gmail.com>
2018-06-26 10:20:24 -0300

    Criando a classe Activity Principal

---
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/activity/PrincipalActivity.java
@@ -0,0 +1,42 @@
+package br.edu.iff.pooa20181.trabalho02_2018_1.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import br.edu.iff.pooa20181.trabalho02_2018_1.R;
+
+public class PrincipalActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    private String[] activities = {"ListaEleitorActivity"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, activities);
+
+        ListView listView = (ListView)findViewById(R.id.listaMenu);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = null;
+
+        try {
+            Class obj =    Class.forName("br.edu.iff.pooa20181.trabalho02_2018_1.activity." + activities[position]);
+            intent = new Intent(this, obj);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        startActivity(intent);
+    }
+}
