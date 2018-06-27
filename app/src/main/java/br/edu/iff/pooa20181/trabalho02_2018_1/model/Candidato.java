2f080bfea92d754a31edd161e3ef1c40891bced0
Lucas <lucasmisidoro@gmail.com>
2018-06-27 07:55:18 -0300

    Criacao das classes Model Candidato e Eleitor

---
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/model/Candidato.java
@@ -0,0 +1,115 @@
+package br.edu.iff.pooa20181.trabalho02_2018_1.model;
+
+import java.io.Serializable;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Candidato extends RealmObject implements Serializable {
+
+
+    @PrimaryKey
+    private int id;
+    private String nome;
+    private String partido;
+    private String numeroUrna;
+    private String cargo;
+    private String numeroVotos;
+    private String estado;
+    private String municipio;
+
+
+    public Candidato(){
+
+    }
+
+    public Candidato(int id, String nome, String partido, String numeroUrna, String cargo)
+    {
+        this.id = id;
+        this.nome = nome;
+        this.partido = partido;
+        this.numeroUrna = numeroUrna;
+        this.cargo = cargo;
+    }
+
+    public Candidato(int id, String nome, String partido, String numeroUrna,
+                     String cargo, String estado, String numeroVotos, String municipio)
+    {
+        this.id = id;
+        this.nome = nome;
+        this.partido = partido;
+        this.numeroUrna = numeroUrna;
+        this.cargo = cargo;
+        this.estado = estado;
+        this.numeroVotos = numeroVotos;
+        this.municipio = municipio;
+
+    }
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPartido() {
+        return partido;
+    }
+
+    public void setPartido(String partido) {
+        this.partido = partido;
+    }
+
+    public String getNumeroUrna() {
+        return numeroUrna;
+    }
+
+    public void setNumeroUrna(String numeroUrna) {
+        this.numeroUrna = numeroUrna;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getNumeroVotos() {
+        return numeroVotos;
+    }
+
+    public void setNumeroVotos(String numeroVotos) {
+        this.numeroVotos = numeroVotos;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+}
--- app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/model/Eleitor.java
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/model/Eleitor.java
@@ -1,25 +1,49 @@
 package br.edu.iff.pooa20181.trabalho02_2018_1.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class Eleitor {
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Eleitor extends RealmObject implements Serializable{
 
 
-
+    @PrimaryKey
     private int id;
     private String nome;
     private String nomeMae;
     private Date dataNascimento;
+    private String numeroTitulo;
+    private String zona;
+    private String secao;
+    private String municipio;
 
     public Eleitor(){
 
     }
 
-    public Eleitor(int id, String nome, String nomeMae, Date dataNascimento){
+    public Eleitor(int id, String nome, String numeroTitulo, String zona, String secao){
+
+        this.id = id;
+        this.nome = nome;
+        this.numeroTitulo = numeroTitulo;
+        this.zona = zona;
+        this.secao = secao;
+
+    }
+
+    public Eleitor(int id, String nome, String nomeMae,Date dataNascimento,
+                   String numeroTitulo, String zona, String secao, String municipio){
+
         this.id = id;
         this.nome = nome;
         this.nomeMae = nomeMae;
         this.dataNascimento = dataNascimento;
+        this.numeroTitulo = numeroTitulo;
+        this.zona = zona;
+        this.secao = secao;
+        this.municipio = municipio;
 
     }
 
@@ -49,6 +73,37 @@ public class Eleitor {
 
     public Date getDataNascimento() {
         return dataNascimento;
+    }
+    public String getNumeroTitulo() {
+        return numeroTitulo;
+    }
+
+    public void setNumeroTitulo(String numeroTitulo) {
+        this.numeroTitulo = numeroTitulo;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+
+    public String getSecao() {
+        return secao;
+    }
+
+    public void setSecao(String secao) {
+        this.secao = secao;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
     }
 
     public void setDataNascimento(Date dataNascimento) {
